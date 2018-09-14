554c4183636a1c6e52657351f8c535ee2f71b5b6
Agustin Carnevale <agustin@Agustins-MacBook-Air.local>
2018-09-14 02:11:22 -0300

    upload image, clean up ignored files

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://agustin387:blog121215@ds161950.mlab.com:61950/blog_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
